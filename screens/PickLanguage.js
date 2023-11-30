9896139616535e395b80b6baa1ec93dae1e95317
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-30 15:05:14 +0100

    feat: picklanguage

--- screens/PickLanguage.js
+++ screens/PickLanguage.js
@@ -1,21 +1,49 @@
+import { useState } from 'react';
 import React from 'react';
-import { Button, Image, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-
 export default function PickLanguage({ navigation }) {
-  return (
-    <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-            <Image
-            style={styles.logo}
-            source={{ uri: 'https://via.placeholder.com/170' }}
-            />
-            <Button title="Nederlands" onPress={() => styles.buttonselected}></Button>
-            <Button title="English" onPress={() => styles.buttonselected}></Button>
-            <Button title=">" onPress={() => navigation.navigate('LogIn')}/>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+    const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+    const handleLanguageSelect = (language) => {
+        setSelectedLanguage(language);
+    };
+
+    const handleNext = () => {
+        navigation.navigate('LogIn', { language: selectedLanguage });
+    };
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <Image
+                    style={styles.logo}
+                    source={{ uri: 'https://via.placeholder.com/170' }}
+                />
+                <View style={styles.languageview}>
+                    <TouchableOpacity
+                        style={selectedLanguage === 'Nederlands' ? styles.buttonSelected : styles.button}
+                        onPress={() => handleLanguageSelect('Nederlands')}
+                    >
+                        <Text style={styles.buttonText}>Nederlands</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={selectedLanguage === 'English' ? styles.buttonSelected : styles.button}
+                        onPress={() => handleLanguageSelect('English')}
+                    >
+                        <Text style={styles.buttonText}>English</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                    style={[styles.bottomRight]}
+                    onPress={handleNext}
+                    disabled={!selectedLanguage}
+                >
+                    <Text style={styles.buttonText}>&gt;</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
 const styles = StyleSheet.create({
     logo: {
@@ -28,7 +56,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    buttonselected: {
+    buttonSelected: {
         backgroundColor: 'red',
     },
+    button: {
+        backgroundColor: '#D9D9D9',
+        borderRadius: 10,
+        paddingTop: 12,
+        paddingBottom: 12,
+        paddingLeft: 110,
+        paddingRight: 110,
+        marginBottom: 32,
+    },
+    buttonText: {
+        color: 'black',
+        textAlign: 'center',
+    },
+    languageview: {
+        marginTop: 20,
+    },
+    bottomRight: {
+        position: 'absolute',
+        right: 32,
+        bottom: 32,
+        backgroundColor: '#D9D9D9',
+        borderRadius: 10,
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 44,
+        paddingRight: 44,
+      },
 });

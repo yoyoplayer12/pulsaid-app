4a03667c13a03faef49c59afb039f2237f8752d4
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-11-30 11:46:34 +0100

    feat: small notification

--- App.js
+++ App.js
@@ -1,22 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect } from 'react';
+
 
 const Stack = createStackNavigator();
 
 
 function HomeScreen() {
+  const scheduleNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Test Notification 📬",
+        body: 'This is a test notification!',
+      },
+      trigger: { seconds: 2 },
+    });
+  };
   return (
     <View style={styles.container}>
       <Text></Text>
       <StatusBar style="auto" />
+      <Button title="Schedule Notification" onPress={scheduleNotification} />
     </View>
   );
 }
 
 
 export default function App() {
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+    Notifications.requestPermissionsAsync()
+    .then(status => console.log(status))
+    .catch(err => console.log(err));
+  }, []); 
+  const scheduleNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Test Notification 📬",
+        body: 'This is a test notification!',
+      },
+      trigger: { seconds: 2 },
+    });
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Install instructions:
 - npm install @react-navigation/native
 - expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
 - npm install @react-navigation/stack
+- expo install expo-notifications
